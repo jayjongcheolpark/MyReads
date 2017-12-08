3d32511195630f23186e5ed6f87c51e522675db4
Jay Park <jay.jongcheol.park@gmail.com>
2017-12-08 17:25:34 -0500

    Optimistic update when changing shelf

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,20 @@ import { combineReducers } from 'redux'
 import {
   FETCH_ALL_BOOKS_SUCCESS,
   SEARCH_BOOKS_SUCCESS,
-  CLEAR_SEARCH_RESULT
+  CLEAR_SEARCH_RESULT,
+  CHANGE_BOOK_SHELF
 } from '../constants/actionTypes'
 
 const bookReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_BOOKS_SUCCESS:
       return [ ...action.books ]
+    case CHANGE_BOOK_SHELF: /* optimistic update */
+      const index = state.indexOf(action.payload.book)
+      const items = state.splice(index, 1)
+      items[0].shelf = action.payload.shelf
+      state.push(items[0])
+      return [ ...state ]
     default:
       return state
   }
