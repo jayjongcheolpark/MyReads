1c49ef1295c25aa4de4c4e30d96d557e839d80c1
Jay Park <jay.jongcheol.park@gmail.com>
2017-12-08 11:58:16 -0500

    Convention for structuring a React component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,6 +6,11 @@ import SearchBar from '../components/SearchBar'
 import SearchResult from '../components/SearchResult'
 
 class Search extends Component {
+
+  componentWillUnmount() {
+    this.props.clearSearchResult()
+  }
+
   throttledSearch = (query) => {
     if (query.trim() === "") {
       this.props.clearSearchResult()
@@ -13,11 +18,6 @@ class Search extends Component {
       this.props.searchBooks(query)
     }
   }
-
- componentWillUnmount() {
-    this.props.clearSearchResult()
-  }
-
 
   render () {
     return (
